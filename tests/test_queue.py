62ccace656561067db42a3598c127272ff875654
lynnAlpha <trainert@163.com>
2016-07-08 22:16:45 +0800

    update queue test

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -6,5 +6,34 @@ sys.path.insert(0, "..")
 
 from adt.queue import Queue
 
+
+def testEnqueueQueue(queue, nodes):
+    for n in nodes:
+        queue.enqueue(n)
+    print("queue enqueue ok")
+
+
+def testPrintQueue(queue):
+    print("queue:", end=' ')
+    while(queue.size() > 0):
+        print(queue.dequeue(), end=' ')
+    print("")
+
+
+def testSizeQueue(queue):
+    print("queue size is: ", queue.size())
+
+
+def testEmptyQueue(queue):
+    print("queue is empty: ", queue.isEmpty())
+
 if __name__ == "__main__":
-    pass
+    q = Queue()
+
+    testEnqueueQueue(q, ['1', '2', '3', 4, '5'])
+
+    testSizeQueue(q)
+
+    testPrintQueue(q)
+
+    testEmptyQueue(q)
